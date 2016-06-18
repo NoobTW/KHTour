da449d1b2da52f40cdd37cbbecfb179f9c5511f8
NoobTW <noob@noob.tw>
2016-06-18 13:31:09 +0800

    Fixed: wrong path

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="./api/jquery-ui-1.11.4/jquery-ui.min.css" />
 	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" />
-	<link rel="stylesheet" href="./stylesheet/leaflet.awesome-markers.css" />
-	<link rel="stylesheet" href="./stylesheet/leaflet.css"/>
+	<link rel="stylesheet" href="./css/leaflet.awesome-markers.css" />
+	<link rel="stylesheet" href="./css/leaflet.css"/>
 	<link rel="stylesheet" href="./style.css"/>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-	<script src="./js/leaflet.awesome-markers.js"></script>
-	<script src="./js/leaflet.js"></script>
+	<script src="./leaflet.js"></script>
+	<script src="./leaflet.awesome-markers.js"></script>
 	<script src="./js/all.js"></script>
 </head>
 <body>
