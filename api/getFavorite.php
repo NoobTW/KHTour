f149d8a470cb18f5f64950377df068b0f2d33b28
John <aver803bath261@gmail.com>
2016-06-20 02:01:51 +0800

    slight fix getFavorite

--- api/getFavorite.php
+++ api/getFavorite.php
@@ -6,10 +6,10 @@ $userId=$_SESSION['user_id'];
 $sth = $dbh->query("SELECT f_target FROM `favorite` WHERE f_user='$userId'", PDO::FETCH_ASSOC);
 $rows = $sth->fetchAll();
 foreach ($rows as $value) {
-	$sth = $dbh->query("SELECT obj_NAME FROM `object` WHERE obj_Id='$value[f_target]'", PDO::FETCH_ASSOC);
+	$sth = $dbh->query("SELECT obj_Name FROM `object` WHERE obj_Id='$value[f_target]'", PDO::FETCH_ASSOC);
 	$rows_target_name = $sth->fetchAll();
 	foreach ($rows_target_name as $Name) {
-		$Names[]=$Name[obj_NAME];
+		$Names[]=$Name[obj_Name];
 	}
 }
 $Names=json_encode($Names);
